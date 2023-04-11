621d2b8b90e81ea3d647bd74e7f5958883a386dc
spaceee123 <space6k@gmail.com>
2023-04-11 16:25:07 +0300

    card

--- store/src/components/Main/CardItem/CardItem.js
+++ store/src/components/Main/CardItem/CardItem.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './CardItem.module.css'
 import apple from '../../../assets/img/Apple.png'
+import airpods from '../../../assets/img/airpods.png'
 
 const CardItem = () => {
+
     return (
-            <div className={styles.cardItem}>
-                <div className={styles.cardHeader}>
-                    <img src={apple} width={"6%"} height={"5%"} style={styles.cardLogo} alt="error"/>
-                    <p></p>
-                </div>
-                <div className={styles.cardPhoto}>
-                    <img src="" alt=""/>
-                </div>
-                <div style={styles.cardFooter}>
-                    <p>
-                        
-                    </p>
-                    <img src="" alt=""/>
-                </div>
+        <div className={styles.cardItem}>
+            <div className={styles.cardHeader}>
+                <img src={apple} width={"20"} style={styles.cardLogo} alt="error"/>
+                <p></p>
             </div>
+            <div className={styles.cardPhoto}>
+                <img src={airpods} alt=""/>
+            </div>
+            <div className={styles.cardFooter}>
+                <p className={styles.cardTitle}>
+                    Airpods
+                </p>
+                <div className={styles.cardInfo}>
+                    <div className={styles.cardInfoTitle}>
+                        <p>Brand</p>
+                        <p>Model</p>
+                        <p>Color</p>
+                        <p>Type</p>
+                    </div>
+                    <div className={styles.cardInfoAbout}>
+                        <p className={styles.cardInfoAboutItem}>Apple</p>
+                        <p className={styles.cardInfoAboutItem}>Color</p>
+                        <p className={styles.cardInfoAboutItem}>white</p>
+                        <p className={styles.cardInfoAboutItem}>Headphones</p>
+                    </div>
+
+                </div>
+
+                <img src="" alt=""/>
+            </div>
+        </div>
     );
 };
 
--- store/src/components/Main/Main.js
+++ store/src/components/Main/Main.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styles from './Main.module.css'
-import  '../../App.css'
+import '../../App.css'
 import Header from "../Header/Header";
 import CardItem from "./CardItem/CardItem";
 
 const Main = () => {
     return (
         <div className={styles.container}>
+
             <Header/>
             <body className={'App_background'}>
-               <CardItem/>
+            <div className={styles.wrapper}>
+                <div className={styles.cardContainer}>
+                    <CardItem/>
+                </div>
+            </div>
             </body>
+
         </div>
     );
 };
