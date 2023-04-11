d824249f048b4080f92a8d4db26dea313247adaa
spaceee123 <space6k@gmail.com>
2023-04-11 14:46:23 +0300

    logo

--- store/src/components/Main/CardItem/CardItem.js
+++ store/src/components/Main/CardItem/CardItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './CardItem.module.css'
+import apple from '../../../assets/img/Apple.png'
 
 const CardItem = () => {
     return (
         <div className={styles.cardContainer}>
             <div className={styles.cardItem}>
                 <div className={styles.cardHeader}>
-                    <img src="" alt=""/>    
+                    <img src={apple} width={"200"} height={"100"} style={styles.cardLogo} alt="error"/>
                     <p></p>
                 </div>
                 <div className={styles.cardPhoto}>
--- store/src/components/Main/Main.js
+++ store/src/components/Main/Main.js
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from './Main.module.css'
 import  '../../App.css'
 import Header from "../Header/Header";
+import CardItem from "./CardItem/CardItem";
 
 const Main = () => {
     return (
         <div className={styles.container}>
             <Header/>
             <body className={'App_background'}>
-                asd
+               <CardItem/>
             </body>
         </div>
     );
